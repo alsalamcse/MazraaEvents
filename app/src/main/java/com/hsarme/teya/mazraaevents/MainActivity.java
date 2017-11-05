83aadeb897c2fc7229da1ae946bba403f5b67c37
user <tya.hsarme@gmail.com>
2017-11-05 13:22:09 +0200

    ..

--- app/src/main/java/com/hsarme/teya/mazraaevents/MainActivity.java
+++ app/src/main/java/com/hsarme/teya/mazraaevents/MainActivity.java
@@ -4,16 +4,33 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnCreateContextMenuListener{
     private Button btnEvents;
+    private Spinner spinner1, spinner2;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner1=(Spinner) findViewById(R.id.spinner1);
+        spinner2=(Spinner) findViewById(R.id.spinner2);
+        btnSubmit=(Button) findViewById(R.id.btnSubmit);
+        addItemsOnSpinner2();
+        addListenerOnButton();
+        addListenerOnSpinnerItemSelection();
+    
+
+        
         btnEvents=(Button) findViewById(R.id.btnEvents);
         btnEvents.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,5 +39,69 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    private void addListenerOnSpinnerItemSelection() {
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
+
+    private void addListenerOnButton() {
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this,
+                        "OnClickListener : " +
+                                "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()) +
+                                "\nSpinner 2 : "+ String.valueOf(spinner2.getSelectedItem()),
+                        Toast.LENGTH_SHORT).show();
+        }
+
+
+    private void addItemsOnSpinner2() {
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        List<String> list = new ArrayList<String>();
+        list.add("list 1");
+        list.add("list 2");
+        list.add("list 3");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(dataAdapter);
+    }
+            public void addListenerOnSpinnerItemSelection() {
+                spinner1 = (Spinner) findViewById(R.id.spinner1);
+                spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+            }
+
+            // get the selected dropdown list value
+            public void addListenerOnButton() {
+
+                spinner1 = (Spinner) findViewById(R.id.spinner1);
+                spinner2 = (Spinner) findViewById(R.id.spinner2);
+                btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+                btnSubmit.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+
+                        Toast.makeText(MainActivity.this,
+                                "OnClickListener : " +
+                                        "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()) +
+                                        "\nSpinner 2 : "+ String.valueOf(spinner2.getSelectedItem()),
+                                Toast.LENGTH_SHORT).show();
+                    }
+
+                });
+    }
+
+
+}
     }
 }
