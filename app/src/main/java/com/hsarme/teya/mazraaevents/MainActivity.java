99d1190246536a346e5d04499efcbca401fba368
user <tya.hsarme@gmail.com>
2017-11-05 12:27:40 +0200

    ..

--- app/src/main/java/com/hsarme/teya/mazraaevents/MainActivity.java
+++ app/src/main/java/com/hsarme/teya/mazraaevents/MainActivity.java
@@ -1,13 +1,26 @@
 package com.hsarme.teya.mazraaevents;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnEvents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnEvents=(Button) findViewById(R.id.btnEvents);
+        btnEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getBaseContext(), Events.class);
+                startActivity(i);
+            }
+        });
     }
 }
