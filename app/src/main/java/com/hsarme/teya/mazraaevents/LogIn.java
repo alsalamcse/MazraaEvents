2b5b89b2171ca95320f2331b13ced836ff4142fb
user <tya.hsarme@gmail.com>
2017-11-05 11:51:42 +0200

    ..

--- app/src/main/java/com/hsarme/teya/mazraaevents/LogIn.java
+++ app/src/main/java/com/hsarme/teya/mazraaevents/LogIn.java
@@ -1,52 +1,130 @@
 package com.hsarme.teya.mazraaevents;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LogIn extends AppCompatActivity {
+public class LogIn extends AppCompatActivity implements View.OnClickListener{
     private EditText etEmail,etPassword;
     private Button btnIn,btnUp;
     private FirebaseAuth auth;
     private FirebaseUser firebaseUser;
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
-        etEmail=(EditText)findViewById(R.id.etEmail);
-        etPassword=(EditText)findViewById(R.id.etPassword);
-        btnIn=(Button) findViewById(R.id.btnIn);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnIn = (Button) findViewById(R.id.btnIn);
         btnIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i =new Intent(getBaseContext(),MainActivity.class);
+                Intent i = new Intent(getBaseContext(), MainActivity.class);
                 startActivity(i);
             }
         });
 
-       btnUp=(Button) findViewById(R.id.btnUp);
+        btnUp = (Button) findViewById(R.id.btnUp);
         btnUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i =new Intent(getBaseContext(),SignUp.class);
+                Intent i = new Intent(getBaseContext(), SignUp.class);
                 startActivity(i);
             }
         });
         auth = FirebaseAuth.getInstance();
         firebaseUser = auth.getCurrentUser();
+    }
 
+    private void dataHandler()
+    {
+        String stemail=etEmail.getText().toString();
+        String stPaswword=etPassword.getText().toString();
+        boolean isOk= true;
+        if(stemail.length()==0|| stemail.indexOf('@')<1)
+        {
+            etEmail.setError("wrong email");
+            isOk=false;
+        }
+        if (stPaswword.length()<8)
+        {
+            etPassword.setError("bad password");
+            isOk=false;
+        }
+        if (isOk)
+        {
+            creatAcount(stemail,stPaswword);
+
+        }
+    }
+
+
+    private void creatAcount(String stemail, String stPaswword) {
+        auth.createUserWithEmailAndPassword(stemail,stPaswword).addOnCompleteListener(LogIn.this, new OnCompleteListener<AuthResult>() {
+
+
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task)
+            {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(LogIn.this, "Authentication Successful.", Toast.LENGTH_SHORT).show();
+
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(LogIn.this, "Authentication failed."+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+                }
+            }
+        });
+        FirebaseAuth.getInstance().signOut();
 
 
 
     }
 
-
+    @Override
+    public void onClick(View view) {
+        if(btnIn==view)
+        dataHandler();
     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- app/src/main/java/com/hsarme/teya/mazraaevents/SignUp.java
+++ app/src/main/java/com/hsarme/teya/mazraaevents/SignUp.java
@@ -1,13 +1,24 @@
 package com.hsarme.teya.mazraaevents;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class SignUp extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUp extends AppCompatActivity implements View.OnClickListener{
     private EditText etName,etemail,etpassword,etPhone;
     private Button btnsave;
+    private FirebaseAuth auth;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +29,71 @@ public class SignUp extends AppCompatActivity {
         etpassword=(EditText)findViewById(R.id.etpassword);
         etPhone=(EditText)findViewById(R.id.etPhone);
         btnsave=(Button) findViewById(R.id.btnsave);
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dataHandler();
+
+            }
+        });
+        auth = FirebaseAuth.getInstance();
+        firebaseUser = auth.getCurrentUser();
+
     }
+    private void dataHandler()
+    {
+        String stemail=etemail.getText().toString();
+        String stPaswword=etpassword.getText().toString();
+        String name=etName.getText().toString();
+        String phone=etPhone.getText().toString();
+        boolean isOk= true;
+        if(stemail.length()==0|| stemail.indexOf('@')<1)
+        {
+            etemail.setError("wrong email");
+            isOk=false;
+        }
+        if (stPaswword.length()<8)
+        {
+            etpassword.setError("bad password");
+            isOk=false;
+        }
+        if (isOk)
+        {
+            creatAcount(stemail,stPaswword);
+
+        }
+    }
+    private void creatAcount(String email, String passw) {
+        auth.createUserWithEmailAndPassword(email,passw).addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+
+
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task)
+            {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(SignUp.this, "Authentication Successful.", Toast.LENGTH_SHORT).show();
+
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(SignUp.this, "Authentication failed."+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+                }
+            }
+        });
+        FirebaseAuth.getInstance().signOut();
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if(btnsave==view){
+            dataHandler();
+    }
+}
 }
