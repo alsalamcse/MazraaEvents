8a704b4302dd242ccfe3f810ff2a0f3934ba319a
user <tya.hsarme@gmail.com>
2018-02-25 13:19:06 +0200

    ..

--- app/src/main/java/com/hsarme/teya/mazraaevents/MainActivity.java
+++ app/src/main/java/com/hsarme/teya/mazraaevents/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        datePicker = (DatePicker) findViewById(R.id.datePicker);
         et = (EditText) findViewById(R.id.et);
         btnSubmit = (Button) findViewById(R.id.btnSubmit);
         btnSubmit.setOnClickListener(new View.OnClickListener() {
@@ -73,7 +72,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
                 calendar.get(Calendar.DAY_OF_MONTH)).show();
 
-                private void updateLabel() {
+                private void updateLabel()
+    {
                     String myFormat = "MM/dd/yy"; //In which you need put here
                     SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 
