05ee3f3104d625badf5b404689880efcce7920b7
user <tya.hsarme@gmail.com>
2018-03-19 13:22:45 +0200

    ..

--- app/src/main/java/com/hsarme/teya/mazraaevents/MainActivity.java
+++ app/src/main/java/com/hsarme/teya/mazraaevents/MainActivity.java
@@ -3,12 +3,17 @@ package com.hsarme.teya.mazraaevents;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
@@ -18,6 +23,8 @@ import com.google.firebase.auth.FirebaseUser;
 import com.hsarme.teya.mazraaevents.data.Note;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -26,7 +33,13 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity
 {
 
-
+    private static final String TAG = "MainActivity";
+    private Calendar currentCalender = Calendar.getInstance(Locale.getDefault());
+    private SimpleDateFormat dateFormatForDisplaying = new SimpleDateFormat("dd-M-yyyy hh:mm:ss a", Locale.getDefault());
+    private SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("MMM - yyyy", Locale.getDefault());
+    private boolean shouldShow = false;
+    private CompactCalendarView compactCalendarView;
+    private ActionBar toolbar;
     CompactCalendarView compactCalendar;
     private SimpleDateFormat dateFormatMonth=new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
     private ImageButton btnAdd;
