38f4fcb61e5d23b5755a6e5c0986009b0b693529
user <tya.hsarme@gmail.com>
2018-05-30 10:52:29 +0300

    ..

--- app/src/main/java/com/hsarme/teya/mazraaevents/data/NoteAdapter.java
+++ app/src/main/java/com/hsarme/teya/mazraaevents/data/NoteAdapter.java
@@ -28,8 +28,14 @@ public class NoteAdapter extends ArrayAdapter<Note> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View view = LayoutInflater.from(getContext()).inflate(R.layout.note_adapter, parent, false);
         TextView itmEvent = (TextView) view.findViewById(R.id.itmEvent);
+        TextView itmEvent2 = (TextView) view.findViewById(R.id.itmEvent2);
+        TextView itmEvent3 = (TextView) view.findViewById(R.id.itmEvent3);
         Note n = getItem(position);
         itmEvent.setText(n.getNotes());
+        itmEvent2.setText(n.getNotes());
+        itmEvent3.setText(n.getNotes());
+
+
         return view;
 
 
